0b43cdd5f4dc41b393e18225a60d39d29e6ffa51
William Chalk <william@printedproducts.com>
2023-01-05 11:09:18 -0500

    added ProjectList component

---
+++ client/src/components/ProjectList/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const ProjectList = ({projects,title}) =>{
+    if(!projects.length){
+        return <h3>No Projects Active!</h3>;
+    }
+
+    return(
+        <div>
+            <h3>{title}</h3>
+            {projects && 
+            projects.map((project)=>{
+                <div key={project._id}>
+                    <p>
+                        <Link to={`/profile/${project.username}`}>{project.username}</Link>
+                    </p>
+                    <div>
+                        <Link to={`/project/${project._id}`}>
+                            <p>{project.description}</p>
+                            <p>
+                                Comments: {project.comments} || Click to{" "}
+                                {project.commentCount ? "see" : "start"} support!
+                            </p>
+                        </Link>
+                    </div>
+                </div>
+            })}
+        </div>
+    );
+};
+
+export default ProjectList;
