4c27599f2b9657fb4ca47d5b066582ad1c983980
William Chalk <william@printedproducts.com>
2023-01-05 16:00:05 -0500

    adding in clent queries and mutations

--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -11,3 +11,31 @@ query currentUser {
   }
 }
 `;
+
+export const QUERY_PROJECT = gql`
+query project($id : ID!){
+  project(_id:$id){
+    _id
+    projectTitle
+    organizationName
+    projectCategory
+    projectDescription
+    projectGoal
+  }
+}
+`;
+
+export const QUERY_PROJECTS = gql`
+query projects($username:String){
+  projects(username:$username){
+    _id
+    projectTitle
+    organizationName
+    projectCategory
+    projectDescription
+    projectGoal
+  }
+}
+`;
+
+
