be266762f87c6e5bde30cf15f7c476324f794f3c
Shelly Foxworth-Hill <sfhill24@gmail.com>
2023-03-15 14:54:22 -0400

    added slogan

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -33,18 +33,22 @@ const Home = () => {
     <div className="flex-container">
 
       <div className="flex-child magenta">
-        <img src={require("../../src/assets/Donate1.png")} alt="donate pic"/>
+        <img src={require("../../src/assets/Donate1.png")} alt="donate pic" />
       </div>
 
       <div className="flex-child green">
         <p className="site-name-style">MONIED</p>
         <p className="mission-stmt">Welcome to Monied, the fundraising website that is committed to helping non-profit organizations achieve their missions.
-         We provide a platform that enables non-profits to raise funds and connect with donors who share their vision.</p>
+          We provide a platform that enables non-profits to raise funds and connect with donors who share their vision.</p>
         <p className="mission-stmt">Our website offers a range of features to help non-profits create successful campaigns, including customizable campaign pages, social media integration, and donation tracking. At Monied, we strive to create a community of giving that brings together people who are passionate about making a positive impact on the world.
-         We believe that every donation, no matter how small, can make a difference in the lives of those who need it most.</p>
-        <p className="mission-stmt">We invite you to explore our campaigns and join us in supporting non-profit organizations that are making a difference in the world. Whether you're looking to donate, volunteer, or spread the word, your support can help create a better future for us all. 
-        Thank you for choosing Monied as your partner in giving back.</p>
+          We believe that every donation, no matter how small, can make a difference in the lives of those who need it most.</p>
+        <p className="mission-stmt">We invite you to explore our campaigns and join us in supporting non-profit organizations that are making a difference in the world. Whether you're looking to donate, volunteer, or spread the word, your support can help create a better future for us all.
+          Thank you for choosing Monied as your partner in giving back.</p>
+        <div className="mission-stmt">
+        "Small Donations, Big Impact - Monied"
+        </div>
       </div>
+
 
     </div>
   )
