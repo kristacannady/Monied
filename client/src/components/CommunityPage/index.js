d3571124974d72fadb7723bc60b1f032d4536785
Argent <jrargent42@gmail.com>
2023-03-26 19:21:24 -0400

    added ability for favorite project to become empty array instead of query if user not logged in.

--- client/src/components/CommunityPage/index.js
+++ client/src/components/CommunityPage/index.js
@@ -11,7 +11,6 @@ import { ADD_FAVORITE } from '../../graphql/mutations';
 import { FaHeart, FaRegHeart } from 'react-icons/fa';
 import { MdOutlineComment } from 'react-icons/md';
 
-
 const Community = () => {
   //favorite project in category
   const [addFavorite, { error }] = useMutation(ADD_FAVORITE);
@@ -31,12 +30,11 @@ const Community = () => {
 
   //get currentUser info from DB
   const currentUserRes = useQuery(QUERY_CURRENT_USER);
-  const userFavs = currentUserRes.data.getCurrentUser.favorites;
+  const userFavs = currentUserRes.data?.getCurrentUser.favorites || []; // added ability to work even if not logged in.
 
   //get current user favorite projects
   const userFavProjectsId = userFavs.map((userFavs) => userFavs._id);
   console.log(userFavProjectsId);
-
 
   //filter projects to get all education category
   const { loading, data } = useQuery(QUERY_PROJECT_CATEGORY, {
@@ -46,7 +44,6 @@ const Community = () => {
   const projects = data?.getProjectByCategory || [];
 
   const projectIds = projects.map((project) => project._id);
-
 
   const comments = projects.map((project) => project.donations[0]?.commentBody);
 
@@ -65,69 +62,76 @@ const Community = () => {
     );
   }
 
-  const matchProjectIds = userFavProjectsId.filter(idData => projectIds.includes(idData));
+  const matchProjectIds = userFavProjectsId.filter((idData) =>
+    projectIds.includes(idData)
+  );
   console.log(matchProjectIds);
 
   let favIcon = null;
-
 
   return (
     <div className="row justify-content-md-center">
       {projects &&
         projects.map((project) => {
-
           if (matchProjectIds.includes(project._id)) {
-            favIcon = <FaHeart className="fav-btn" size={35} />
-          }
-          else {
-            favIcon = <FaRegHeart className="fav-btn" size={35} />
+            favIcon = <FaHeart className="fav-btn" size={35} />;
+          } else {
+            favIcon = <FaRegHeart className="fav-btn" size={35} />;
           }
 
-          const comments = project.donations.filter(donation => donation.commentBody != null);
+          const comments = project.donations.filter(
+            (donation) => donation.commentBody != null
+          );
           console.log(comments.length);
 
-          return (<div className="col-md-auto d-flex" key={project._id}>
-            <div className="project-card card">
-              <div className="new-project-form card-body">
-                <div className="container">
-                  <div className="row">
-                    <div className="col-sm">
-                      {/* Add link to ProjectsByOrg, need to take link that is clicked and prop into projectsbyorg*/}
+          return (
+            <div className="col-md-auto d-flex" key={project._id}>
+              <div className="project-card card">
+                <div className="new-project-form card-body">
+                  <div className="container">
+                    <div className="row">
+                      <div className="col-sm">
+                        {/* Add link to ProjectsByOrg, need to take link that is clicked and prop into projectsbyorg*/}
 
-                      <Link
-                        className="org-name"
-                        to={`/ProjectsByOrg/${project.organizationName}`}
+                        <Link
+                          className="org-name"
+                          to={`/ProjectsByOrg/${project.organizationName}`}
+                        >
+                          {project.organizationName}
+                        </Link>
+                      </div>
+                      <div className="col-sm">
+                        <MdOutlineComment size={35}></MdOutlineComment>
+                        <span>{comments.length}</span>
+                      </div>
+                      <div
+                        className="col-sm"
+                        onClick={() => favoriteProject(project._id)}
                       >
-                        {project.organizationName}
-                      </Link>
-                    </div>
-                    <div className="col-sm">
-                      <MdOutlineComment size={35}></MdOutlineComment><span>{comments.length}</span>
-                    </div>
-                    <div className="col-sm" onClick={() => favoriteProject(project._id)}>
-                      {favIcon}
+                        {favIcon}
+                      </div>
                     </div>
                   </div>
-                </div>
-                <Link className="project-link" to={`/project/${project._id}`}>
-                  <h3 className="card-title">{project.projectTitle}</h3>
-                </Link>
-                <p className="card-text">{project.projectDescription}</p>
-                <p className="card-text">Goal: ${project.projectGoal}</p>
-                <div className="progress">
-                  <div
-                    className="progress-bar bg-custom w-50"
-                    role="progressbar"
-                    aria-valuenow="75"
-                    aria-valuemin="0"
-                    aria-valuemax="100"
-                  >
-                    50%
+                  <Link className="project-link" to={`/project/${project._id}`}>
+                    <h3 className="card-title">{project.projectTitle}</h3>
+                  </Link>
+                  <p className="card-text">{project.projectDescription}</p>
+                  <p className="card-text">Goal: ${project.projectGoal}</p>
+                  <div className="progress">
+                    <div
+                      className="progress-bar bg-custom w-50"
+                      role="progressbar"
+                      aria-valuenow="75"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    >
+                      50%
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>)
+          );
         })}
     </div>
   );
