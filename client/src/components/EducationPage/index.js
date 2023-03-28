6a31ab6d423176439b95f72c37e7968a2ffa34d8
Shelly Foxworth-Hill <sfhill24@gmail.com>
2023-03-28 15:49:39 -0400

    styled category pages

--- client/src/components/EducationPage/index.js
+++ client/src/components/EducationPage/index.js
@@ -1,24 +1,19 @@
 import React from 'react';
 //figure out how to import projects from database
-import { QUERY_PROJECT_CATEGORY } from '../../graphql/queries';
+import {
+  QUERY_PROJECT_CATEGORY,
+  QUERY_CURRENT_USER,
+} from '../../graphql/queries';
 import { useQuery } from '@apollo/client';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { ADD_FAVORITE } from '../../graphql/mutations';
+import { FaHeart, FaRegHeart } from 'react-icons/fa';
+import { MdOutlineComment } from 'react-icons/md';
+import Logo from "../../assets/Monied-1 (1).png";
+
 
 const Education = () => {
-  //filter projects to get all education category
-
-  // const { loading, error, data } = useQuery(QUERY_PROJECT_CATEGORY);
-  // if (loading){
-  //   return(
-  //     <div>Loading...</div>
-  //   );
-  // }
-  // if (error){
-  //   return `error! ${error.message}`;
-  // }
-
   //favorite project in category
   const [addFavorite, { error }] = useMutation(ADD_FAVORITE);
 
@@ -35,17 +30,33 @@ const Education = () => {
     }
   };
 
+  //filter projects to get all education category
   const { loading, data } = useQuery(QUERY_PROJECT_CATEGORY, {
     variables: { projectCategory: 'Education' },
   });
 
-  const projects = data?.getProjectByCategory || [];
+  //get currentUser info from DB
+  const currentUserRes = useQuery(QUERY_CURRENT_USER);
 
-  console.log();
-
-  if (loading) {
+  if (loading || currentUserRes.loading) {
     return <div className="no-projects-message">Loading...</div>;
   }
+
+  const notLoggedIn = currentUserRes.data?.getCurrentUser == null;
+
+  const userFavs = currentUserRes.data?.getCurrentUser?.favorites;
+
+  //get current user favorite projects
+  const userFavProjectsId = userFavs?.map((userFavs) => userFavs._id);
+  console.log(userFavProjectsId);
+
+  const projects = data.getProjectByCategory || [];
+
+  const projectIds = projects.map((project) => project._id);
+
+  const comments = projects.map((project) => project.donations?.commentBody);
+
+  const MAX_LENGTH = 60;
 
   if (projects.length === 0) {
     return (
@@ -56,36 +67,95 @@ const Education = () => {
     );
   }
 
+  const matchProjectIds = userFavProjectsId?.filter(idData => projectIds.includes(idData));
+  console.log(matchProjectIds);
+
+  let favIcon = null;
+
   return (
     <div className="row justify-content-md-center">
       {projects &&
-        projects.map((project) => (
-          <div className="col-md-auto d-flex" key={project._id}>
-            <div className="card">
+        projects.map((project) => {
+
+          if (notLoggedIn) {
+            favIcon = null;
+          }
+          else if (matchProjectIds?.includes(project._id)) {
+            favIcon = <FaHeart className="fav-btn" size={35} />
+          }
+          else {
+            favIcon = <FaRegHeart className="fav-btn" size={35} />
+          }
+
+          //Truncate description length 
+          let trimmedDescription = project.projectDescription;
+
+          if (trimmedDescription.length > MAX_LENGTH) {
+            trimmedDescription = project.projectDescription.slice(0, MAX_LENGTH) + '...';
+          }
+
+          const comments = project.donations.filter(donation => donation.commentBody != null);
+
+          const donationValues = project.donations.map((donation) => donation.donationAmount);
+          console.log(donationValues);
+
+          //const comments = projects.map((project) => project.donations?.commentBody);
+
+          const totalDonations = donationValues.reduce((accumulator, currentValue) => {
+            return accumulator + currentValue;
+          }, 0);
+
+          console.log(donationValues);
+
+          return (<div className="col-md-auto d-flex" key={project._id}>
+            <div className="project-card card">
               <div className="new-project-form card-body">
-                <Link to={`/project/${project._id}`}>
-                  <h3 className="card-title">{project.projectTitle}</h3>
-                </Link>
-                <p className="card-text">
-                  Organization: {project.organizationName}
-                </p>
-                <p className="card-text">Category: {project.projectCategory}</p>
-                <p className="card-text">
-                  Description: {project.projectDescription}
-                </p>
-                <p className="card-text">
-                  Donations Raised: {project.projectGoal}
-                </p>
-                <button
-                  className="btn btn-light"
-                  onClick={() => favoriteProject(project._id)}
-                >
-                  Add to Favorites
-                </button>
+                <div className="container">
+                  <div className="row" id="card-icon">
+                    <div className="col-sm">
+                      {/* Add link to ProjectsByOrg, need to take link that is clicked and prop into projectsbyorg*/}
+                      <Link
+                        className="org-name org-name-style hover"
+                        to={`/ProjectsByOrg/${project.organizationName}`}
+                      >
+                        {project.organizationName}
+                      </Link>
+                    </div>
+                    <div className="col-sm">
+                      <MdOutlineComment size={35}></MdOutlineComment><span>{comments.length}</span>
+                    </div>
+                    <div className="col-sm" onClick={() => favoriteProject(project._id)}>
+                      {favIcon}
+                    </div>
+                  </div>
+                </div>
+                <div className='row .card-row'>
+                  <div className="col-md">
+                    <img src={Logo} className="rounded float-left user-image-card" alt="..."></img>
+                  </div>
+                  <div className="col-md">
+                    <Link className="project-link" to={`/project/${project._id}`}>
+                      <h3 className="card-title">{project.projectTitle}</h3>
+                    </Link>
+                    <p className="card-text">{trimmedDescription}</p> 
+                    <p className="card-text goal-card-text">${totalDonations} raised of ${project.projectGoal} goal</p>
+                    <div className="progress">
+                      <div
+                        className="progress-bar bg-custom w-50"
+                        role="progressbar"
+                        aria-valuenow="75"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >
+                        50%
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          </div>)
+        })}
     </div>
   );
 };
